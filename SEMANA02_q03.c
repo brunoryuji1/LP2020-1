12a5b5acafe872f7539c5a24ff9e4f05de5e981c
brunoryuji1 <brunosampaio027@gmail.com>
2021-03-07 21:56:17 +0000

    LP - Exerc

--- SEMANA02_q03.c
+++ SEMANA02_q03.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(){
-	int a, b, c;
+// int main(){
+// 	int a, b, c;
 	
-	scanf("%i %i %i", &a, &b, &c);
+// 	scanf("%i %i %i", &a, &b, &c);
 	
-	if((a>=b+c) || (b>=a+c) || (c>=a+b)){
-		printf("NAO FORMA");
-		}else{
-			if ((a==b) && (a==c)){
-			printf("EQUILATERO");
-				}else{
-					if((a==b) || (a==c) || (b==c)){
-					printf("ISOSCELES");	
-					}else{
-						if((a!=b && a!=c) && (b!=a && b!=c) && (c!=a && c!=b)){
-             			printf("ESCALENO");
-             		}
-				}
-			}
-		}
-	return 0;
-}
+// 	if((a>=b+c) || (b>=a+c) || (c>=a+b)){
+// 		printf("NAO FORMA");
+// 		}else{
+// 			if ((a==b) && (a==c)){
+// 			printf("EQUILATERO");
+// 				}else{
+// 					if((a==b) || (a==c) || (b==c)){
+// 					printf("ISOSCELES");	
+// 					}else{
+// 						if((a!=b && a!=c) && (b!=a && b!=c) && (c!=a && c!=b)){
+//              			printf("ESCALENO");
+//              		}
+// 				}
+// 			}
+// 		}
+// 	return 0;
+// }
---
+++ SEMANA02_q04.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+	int A = 1, B = 2, C = 3, D = 4, prof1, prof2, prof3;
+	
+	scanf("%i %i %i", &prof1, &prof2, &prof3);
+	
+		if((prof1 == A && prof2 == A) || (prof1 == A && prof3 == A) || (prof2 == A && prof3 == A)){
+		printf("APROVADO COM A\n");
+			}else{
+			if((prof1 == B && prof2 == B) || (prof1 == B && prof3 == B) || (prof2 == B && prof3 == B)){
+			printf("APROVADO COM B\n");		
+				}else{
+					if((prof1 == C && prof2 == C) || (prof1 == C && prof3 == C) || (prof2 == C && prof3 == C)){
+					printf("APROVADO COM C\n");
+					}else{
+						if((prof1 == D && prof2 == D) || (prof1 == D && prof3 == D) || (prof2 == D && prof3 == D)){
+						printf("REPROVADO\n");
+						}else{
+							if((prof1 == A && prof2 == B && prof3 == C) || (prof1 == B && prof2 == A && prof3 == C) || (prof1 == C && prof2 == B && prof3 == A) || (prof1 == B && prof2 == C && prof3 == A) || (prof1 == C && prof2 == A && prof3 == B) || (prof1 == A && prof2 == C && prof3 == B)){
+							printf("INCONCLUSIVO\n");	
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
