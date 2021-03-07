86b207f1cc2229a9d17d169696ed1e50f36ddb2d
brunoryuji1 <brunosampaio027@gmail.com>
2021-03-07 15:39:16 +0000

    LP - Exec

--- SEMANA01_q01.c
+++ SEMANA01_q01.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(){
-	float valor, r;
+// int main(){
+// 	float valor, r;
 	
-	scanf("%f", &valor);
+// 	scanf("%f", &valor);
 	
-	if(valor <= 100){
-		r = valor - (valor * 0.00);
-		printf("R$%.2f", r);
-			}else{
-			if(valor > 100 && valor <= 200){
-			r = valor - (valor * 0.05);
-			printf("R$%.2f", r);
-				}else{
-					r = valor - (valor * 0.10);
-					printf("R$%.2f", r);
-			}
-		}
-	return 0;
-}
+// 	if(valor <= 100){
+// 		r = valor - (valor * 0.00);
+// 		printf("R$%.2f", r);
+// 			}else{
+// 			if(valor > 100 && valor <= 200){
+// 			r = valor - (valor * 0.05);
+// 			printf("R$%.2f", r);
+// 				}else{
+// 					r = valor - (valor * 0.10);
+// 					printf("R$%.2f", r);
+// 			}
+// 		}
+// 	return 0;
+// }
---
+++ SEMANA01_q02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	int x, y;
+	
+		scanf("%i %i", &x, &y);
+		printf("%i %i %i %i %i %i", x-1, x, x+1, y-1, y, y+1);
+	
+	return 0;
+}
