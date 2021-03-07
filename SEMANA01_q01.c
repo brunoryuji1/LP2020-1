f96d228c2a1067bd409b44a03d1301a59ca5b420
brunoryuji1 <brunosampaio027@gmail.com>
2021-03-07 15:18:03 +0000

    Exerc - LP

---
+++ SEMANA01_q01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	float valor, r;
+	
+	scanf("%f", &valor);
+	
+	if(valor <= 100){
+		r = valor - (valor * 0.00);
+		printf("R$%.2f", r);
+			}else{
+			if(valor > 100 && valor <= 200){
+			r = valor - (valor * 0.05);
+			printf("R$%.2f", r);
+				}else{
+					r = valor - (valor * 0.10);
+					printf("R$%.2f", r);
+			}
+		}
+	return 0;
+}
